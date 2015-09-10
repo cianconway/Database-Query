1baa5623c2c1fa20942f038842aa2587a79396b8
cianconway <cconway@industrydive.com>
2015-09-10 17:06:08 -0400

    Changed to include information to the user saying when it is connecting or executing an SQL command.

--- Connect.py
+++ Connect.py
@@ -5,11 +5,22 @@ import psycopg2.extensions
 
 DATABASE_HOST = 'ec2-54-204-20-209.compute-1.amazonaws.com'
 DATABASE_NAME = "d5qsnqpppo4nkq"
-SCHEMA = 'postgres'
 USER_NAME = 'edbiaisfnankjt'
-PASSWORD = ' _9F3AzzV1QSm9tEVu3tlrcoGcI'
+PASSWORD = '_9F3AzzV1QSm9tEVu3tlrcoGcI'
 SHOW_WHITE_SPACE = True # set to False if you need to trim column data
 
-sql = """\copy edited_subscribers TO '/tmp/subscribers.csv' DELIMITER ',' CSV HEADER"""
+sql = """copy_from edited_subscribers TO '/tmp/subscribers123.csv' DELIMITER ',' CSV HEADER"""
 
-sql = """SELECT * FROM subscribers WHERE edited=True AND flagged=False"""  # I want to combine both queries
+
+
+
+sys.stderr.write('Connecting to %s...\n' % DATABASE_HOST)
+con = psycopg2.connect(database=DATABASE_NAME, user=USER_NAME, host=DATABASE_HOST, password=PASSWORD)
+
+cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+sys.stderr.write("Executing: '%s'\n" % sql)
+cur.execute(sql)
+
+cur.close()
+con.close()
+
